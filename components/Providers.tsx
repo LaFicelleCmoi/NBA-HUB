ddf69369f2bbbdd5c2e0b07d31a3ab991e2fe0e2
LaFicelleCmoi <lois.clerc@epitech.eu>
2026-09-24 01:26:04 +0200

    providers: retire le provider du favori
    Le favori n'a plus besoin de contexte : il se lit depuis n'importe quel
    composant client.

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import { MotionConfig } from "framer-motion";
-import { FavoriteProvider } from "@/lib/client/favorite";
 import { ThemeProvider } from "@/lib/client/theme";
 
 export function Providers({ children, nonce }: { children: React.ReactNode; nonce?: string }) {
   return (
     <MotionConfig reducedMotion="user" nonce={nonce}>
-      <ThemeProvider>
-        <FavoriteProvider>{children}</FavoriteProvider>
-      </ThemeProvider>
+      <ThemeProvider>{children}</ThemeProvider>
     </MotionConfig>
   );
 }
